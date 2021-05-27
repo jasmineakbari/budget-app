3283c0892617f8ca8c5326a8c1caacf3fc4243c8
jasmineakbari <jasmineakbari@yahoo.com>
2021-05-27 13:59:06 -0400

    created add expense item function and form reset function

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -20,7 +20,7 @@ interface Expenses {
 export class BudgetComponent implements OnInit {
     //define user initial user input and total budget
     input_budget: number = 100;
-	total_budget: number = 0.00;
+	total_budget: number = 100.00;
 
     // presets for expense form
     selected: Expenses = {
@@ -69,5 +69,74 @@ export class BudgetComponent implements OnInit {
 		this.expenses.forEach((item: Expenses) => {
 			item.actual = item.cost * 100 / this.total_budget;
 		});
+	}
+
+    onUpdate(): void {
+		// check validate
+		if ( !this.selected.expense ) {
+			return;
+        }
+
+		if ( this.selected.cost < 1 ) {
+            return;
+        }
+		
+		if ( this.selected.desired < 1 ) {
+            return;
+        }
+			
+        // create
+		if ( this.selected.id < 1 ) {
+		
+			// find max id
+			let max_id = 0;
+			this.expenses.forEach((item: Expenses) => {
+				if( item.id > max_id )
+					max_id = item.id;
+			});
+
+            //assigns input to expense model
+			let model: Expenses = {
+				id: max_id + 1,
+				expense: this.selected.expense,
+				cost: this.selected.cost,
+				priority: this.selected.priority,
+				desired: this.selected.desired,
+				actual: 0
+			};
+
+            //pushes into expense array
+			this.expenses.push(model);
+            // calculate actual percentage 
+			this.calcuateActual();
+			
+			return;
+		}
+
+		let model = this.expenses.find((item: Expenses) => this.selected.id == item.id);
+
+		if ( !model ) {
+			return;
+        }
+
+		model.expense = this.selected.expense;
+		model.cost = this.selected.cost;
+		model.priority = this.selected.priority;
+		model.desired = this.selected.desired;
+
+		this.calcuateActual();
+		this.onCancel();
+	}
+
+    // resets form data
+	onCancel(): void {
+		this.selected = {
+			id: 0,
+			expense: '',
+			cost: 0,
+			priority: 'High',
+			desired: 0,
+			actual: 0
+		}
 	}
 }
