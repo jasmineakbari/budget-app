21f0254e310d01680486dd66c31a21a7f0e0aa04
jasmineakbari <jasmineakbari@yahoo.com>
2021-05-27 13:01:15 -0400

    created starter expense data and presets for form

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -19,8 +19,39 @@ interface Expenses {
 // Export Budget Component Functions
 export class BudgetComponent implements OnInit {
     //define user initial user input and total budget
-    input_budget: number = 0;
+    input_budget: number = 100;
 	total_budget: number = 0.00;
+
+    // presets for expense form
+    selected: Expenses = {
+		id: 0,
+		expense: '',
+		cost: 0,
+		priority: 'High',
+		desired: 0,
+		actual: 0
+	}
+
+    // starter example data on load
+    expenses: Expenses[] = [
+		{
+			id: 1,
+			expense: 'Food',
+			cost: 40,
+			priority: 'High',
+			desired: 30,
+			actual: 40
+		},
+		
+		{
+			id: 2,
+			expense: 'Netflix',
+			cost: 13,
+			priority: 'low',
+			desired: 15,
+			actual: 15
+		}
+	];
 
     constructor() { }
 
@@ -31,6 +62,12 @@ export class BudgetComponent implements OnInit {
     // handles update budget button function
     onUpdateBudget(budget: number): void {
 		this.total_budget = budget;
+        this.calcuateActual();
 	}
     
+    calcuateActual(): void {
+		this.expenses.forEach((item: Expenses) => {
+			item.actual = item.cost * 100 / this.total_budget;
+		});
+	}
 }
