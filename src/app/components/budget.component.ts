843e0797ccc2143a0fd0ae72eaa1aad3cb236406
jasmineakbari <jasmineakbari@yahoo.com>
2021-05-27 07:28:05 -0400

    created table and starter css and defined total budget

--- src/app/components/budget.component.ts
+++ src/app/components/budget.component.ts
@@ -16,10 +16,21 @@ interface Expenses {
 	styleUrls: ['./budget.component.css']
 })
 
+// Export Budget Component Functions
 export class BudgetComponent implements OnInit {
+    //define user initial user input and total budget
+    input_budget: number = 0;
+	total_budget: number = 0.00;
+
     constructor() { }
 
 	ngOnInit(): void {
 
 	}
+
+    // handles update budget button function
+    onUpdateBudget(budget: number): void {
+		this.total_budget = budget;
+	}
+    
 }
